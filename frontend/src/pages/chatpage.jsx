13bf0457cbce1fc0cc3fafd696f7aa1f63401466
DHRUVRAJ SINGH <dhruvrajsingh737@gmail.com>
2026-08-11 19:31:44 +0000

    just add file for first make only 2 user talk in chat application.

---
+++ frontend/src/pages/chatpage.jsx
@@ -0,0 +1,13 @@
+import {useEffect , useState} from "react";
+import {io} from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+export function chatpage() {
+    return(
+    <>
+
+    </>
+    
+)
+}
